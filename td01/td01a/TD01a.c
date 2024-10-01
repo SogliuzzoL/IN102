d2d40e9444ce3b26063c87a9de67963f77ac869e
SOGLIUZZO Loris <sogliuzzo.loris@gmail.com>
2024-10-01 16:43:54 +0200

    TD01

---
+++ td01/td01a/TD01a.c
@@ -0,0 +1,15 @@
+/* Loris SOGLIUZZO */
+#include <stdio.h>
+
+int main()
+{
+    printf("Mon\npremier\nprogramme\n");
+    return 0;
+}
+
+/* 
+Question 2: gcc thefirstone.c
+Question 3: a.out
+Question 4: ./a.out
+Question 5: gcc thefirstone.c -o monprogramme
+*/
